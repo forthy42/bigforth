df675c3512e0af95b50c958b347ef68a0a41704b
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2007-09-19 15:16:30 +0000

    Example added
    git-svn-id: https://forth-ev.de/repos/bigforth@550 3b8d8251-53f3-0310-8f3b-fd1cb8370982

--- i18n.fs
+++ i18n.fs
@@ -21,14 +21,14 @@ Variable lsids
     here lsids @ A, lsids ! lsid# dup , 1+ to lsid# s, ;
 
 : LLiteral  2dup search-lsid dup  IF
-	nip nip postpone (l2) A,
+        nip nip postpone (l2) A,
     ELSE  drop postpone (l") l,  THEN ; immediate
 
 : L" ( "lsid<">" -- lsid ) '" parse
     state @ IF  postpone LLiteral
     ELSE  2dup search-lsid dup  IF
-	    nip nip
-	ELSE  drop align here >r l, r>  THEN
+            nip nip
+        ELSE  drop align here >r l, r>  THEN
     THEN  ; immediate
 
 \ deliberately unique string
@@ -58,17 +58,39 @@ Variable locale-stack  locale-depth cells allot
     nip ;
 
 : locale@ ( lsid -- addr u )  locale-stack @ IF
-	dup >r id#@
-	locale-stack @+ swap cells bounds swap cell- DO
-	    dup I @ search-lsid# dup IF  nip native@ unloop rdrop EXIT  THEN
-	    drop
-	-cell +LOOP  drop r>
+        dup >r id#@
+        locale-stack @+ swap cells bounds swap cell- DO
+            dup I @ search-lsid# dup IF  nip native@ unloop rdrop EXIT  THEN
+            drop
+        -cell +LOOP  drop r>
     THEN  native@ ;
 
-: locale! ( addr u lsid -- )  id#@
-    here locale' dup @ A, ! , s, ;
+: locale! ( addr u lsid -- ) >r
+    2dup r@ locale@ str= IF  rdrop 2drop  EXIT  THEN
+    r> id#@ here locale' dup @ A, ! , s, ;
+
+\ easy use
+
+: x" state @ IF  postpone l" postpone locale@
+    ELSE  ['] l" execute locale@  THEN ;
 
 \ Examples
+
+Create months 12 cells allot
+l" January"   months 0 cells + !
+l" February"  months 1 cells + !
+l" March"     months 2 cells + !
+l" April"     months 3 cells + !
+l" May"       months 4 cells + !
+l" June"      months 5 cells + !
+l" July"      months 6 cells + !
+l" August"    months 7 cells + !
+l" September" months 8 cells + !
+l" October"   months 9 cells + !
+l" November"  months 10 cells + !
+l" December"  months 11 cells + !
+
+: .month ( n -- ) 1- cells months + @ locale@ type ;
 
 ' lsids Alias en
 
@@ -79,14 +101,10 @@ de >locale
 s" Januar" l" January" locale!
 s" Februar" l" February" locale!
 s" März" l" March" locale!
-l" April" drop
 s" Mai" l" May" locale!
 s" Juni" l" June" locale!
 s" Juli" l" July" locale!
-l" August" drop
-l" September" drop
 s" Oktober" l" October" locale!
-l" November" drop
 s" Dezember" l" December" locale!
 
 Locale de_AT
@@ -96,3 +114,37 @@ de_AT >locale
 s" Jänner" l" January" locale!
 
 locale-stack off
+
+Locale fr
+fr >locale
+
+s" Janvier" l" January" locale!
+s" Février" l" February" locale!
+s" Mars" l" March" locale!
+s" Avril" l" April" locale!
+s" Mai" l" May" locale!
+s" Juin" l" June" locale!
+s" Juillet" l" July" locale!
+s" Août" l" August" locale!
+s" Septembre" l" September" locale!
+s" Octobre" l" October" locale!
+s" Novembre" l" November" locale!
+s" Décembre" l" December" locale!
+
+locale-stack off
+
+Locale zh
+zh >locale
+
+s" 一月" l" January" locale!
+s" 二月" l" February" locale!
+s" 三月" l" March" locale!
+s" 四月" l" April" locale!
+s" 五月" l" May" locale!
+s" 六月" l" June" locale!
+s" 七月" l" July" locale!
+s" 八月" l" August" locale!
+s" 九月" l" September" locale!
+s" 十月" l" October" locale!
+s" 十一月" l" November" locale!
+s" 十二月" l" December" locale!
