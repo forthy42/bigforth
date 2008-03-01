03c4b7d43712dc922c7dfb5788f079616ec6d3d2
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2008-03-01 23:43:29 +0000

    Integration added
    git-svn-id: https://forth-ev.de/repos/bigforth@648 3b8d8251-53f3-0310-8f3b-fd1cb8370982

--- fly-by.fs
+++ fly-by.fs
@@ -50,7 +50,7 @@ FVariable phi0  0e phi0 f!
   k f@ epsilon f@ f**2 !1 f- f/ b f!
   k f@ GM f@ f* fsqrt b f@ f/ U f! ;
 
-: r ( phi -- )  fcos epsilon f@ f* !1 f+ k f@ fswap f/ ;
+: r ( phi -- r )  fcos epsilon f@ f* !1 f+ k f@ fswap f/ ;
 
 : range ( -- maxphi )  epsilon f@ 1/f fnegate facos ;
 
@@ -64,3 +64,39 @@ FVariable phi0  0e phi0 f!
   fswap fsin f* diff-a fr> f* ;
 
 : v ( r -- )  GM f@ fswap f/ f2* U f@ f**2 f+ fsqrt ;
+
+\ integration
+
+: phi>pos ( phi -- x y )  fdup r fswap r,phi>xy ;
+
+: delta-step ( maxphi stephpi -- delta-a )
+    fswap fdup r fover phi0 f@ f+ fover delta-a { f: a |
+    v { f: v |
+    fswap fover fover
+    f- phi>pos { f: x1 f: y1 |
+    f+ phi>pos { f: x2 f: y2 |
+    x1 x2 f- f**2 y1 y2 f- f**2 f+ fsqrt
+    v f/ a f*
+    x2 x1 f- y2 y1 f- fatan2 phi0 f@ f+ fcos f* } } } } ;
+
+: integrate ( n -- result )
+  range fdup dup 2* fm/ { f: maxphi f: stepphi } 0e
+    dup negate 1+ DO
+	I abs I' 9 10 */ <  IF
+	    maxphi I I' fm*/ stepphi delta-step f+
+	THEN
+  LOOP ;
+
+: phis ( n m -- )
+    dup negate 1+ ?DO
+	pi I I' 1- fm*/ phi0 f!
+	dup integrate phi0 f@ pi f/ 180e f* f.
+	( phi0 f@ fcos f* ) 1e3 f* f. cr
+    LOOP drop ;
+
+\ Examples:
+
+: galileo  959.9e3 2.47e  orbit2 ;
+: NEAR     538.8e3 1.81e  orbit2 ;
+: cassini  1173e3  5.8e   orbit2 ;
+: rosetta  1954e3  1.327e orbit2 ;
