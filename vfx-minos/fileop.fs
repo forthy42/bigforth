7d6b73d56f6b64311e2cb0ea56e06f479a5922e8
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2009-03-08 22:16:22 +0000

    Fileop for VFX
    git-svn-id: https://forth-ev.de/repos/bigforth@1073 3b8d8251-53f3-0310-8f3b-fd1cb8370982

---
+++ vfx-minos/fileop.fs
@@ -0,0 +1,58 @@
+\ Fileoutput                                           12sep08py
+
+Module FileOP
+
+\ Alle FILEoutput Routinen                             25may97py
+
+1 cells +User fhandle    fhandle off
+1 cells +User oldout
+| Create fchar 0 c,
+| Create crlf  1 c, #lf c,
+: FILEtype ( addr u sid -- ) drop fhandle @ write-file throw ;
+: FILEemit ( char sid -- ) drop fchar tuck c! 1 FILEtype ;
+: FILEcr ( sid -- ) drop  crlf count FILEtype ;
+: FILEpage ( sid -- ) drop FILEcr $0C FILEemit FILEcr ;
+: FILEeot ( sid -- ) drop
+  oldout @ op-handle !  fhandle @ fhandle off close-file throw ;
+
+create filedev-vectors
+  ' drop ,
+  ' FILEeot ,
+  ' drop ,
+  ' drop ,
+  ' drop ,
+  ' drop ,
+  ' drop ,
+  ' drop ,
+  ' drop ,
+  ' FILEemit ,
+  ' drop ,
+  ' FILEtype ,
+  ' FILEcr ,
+  ' drop ,
+  ' FILEpage ,
+  ' drop ,
+  ' drop ,
+  ' drop ,
+  ' drop ,
+  ' drop ,
+  ' drop  ,
+  ' drop ,
+  ' drop ,
+  ' drop ,
+  ' drop ,
+: FILEout  filedev-vectors op-handle ! ;
+
+
+\ 0name >FILE +FILE                                    12mar00py
+
+Module;
+
+also fileop
+
+: output-file ( addr u mode -- )
+  fhandle @ ?dup  IF  close-file drop  fhandle off  THEN
+  create-file throw fhandle !
+  op-handle @ oldout !  FILEout ;
+
+previous
