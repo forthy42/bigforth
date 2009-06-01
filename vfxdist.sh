a8aa7abf93fbffb6829586dc8f6c3f445d11c804
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2009-06-01 22:08:37 +0000

    Added date to VFX MINOS dist
    git-svn-id: https://forth-ev.de/repos/bigforth@1241 3b8d8251-53f3-0310-8f3b-fd1cb8370982

--- vfxdist.sh
+++ vfxdist.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-tar zcf vfx-minos.tar.gz *.m $(for i in *.m; do [ -f ${i%m}fs ] && echo ${i%m}fs; done) $(vfxlin include startx.fs save xvfxlin bye | grep Including | cut -f2 -d' ')
+DATE=$(LC_ALL=C date '+%d%b%Y' | tr '[A-Z]' '[a-z]')
+
+tar zcf vfx-minos-$DATE.tar.gz *.m $(for i in *.m; do [ -f ${i%m}fs ] && echo ${i%m}fs; done) $(vfxlin include startx.fs save xvfxlin bye | grep Including | cut -f2 -d' ') COPYING LGPLv3
