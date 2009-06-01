1789312603a377487a87449f0217be23b9b3e97d
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2009-06-01 22:17:33 +0000

    Added Theseus and minos-float.fs
    git-svn-id: https://forth-ev.de/repos/bigforth@1242 3b8d8251-53f3-0310-8f3b-fd1cb8370982

--- minos-float.fs
+++ minos-float.fs
@@ -2,7 +2,7 @@
 
 [defined] VFXForth [IF]
     mpe-float
-    include /usr/share/doc/VfxForth/Lib/Ndp387.fth
+    include %VFXPATH%/Lib/Ndp387.fth
     ans-float
     : float ;
     : fx$  pad dup f>ascii ;
