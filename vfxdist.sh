52f15cb7de61c0f368c651620e236f838ca48c0b
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2009-06-12 19:02:42 +0000

    Added pattern used in examples
    git-svn-id: https://forth-ev.de/repos/bigforth@1257 3b8d8251-53f3-0310-8f3b-fd1cb8370982

--- vfxdist.sh
+++ vfxdist.sh
@@ -2,5 +2,8 @@
 
 DATE=$(LC_ALL=C date '+%d%b%Y' | tr '[A-Z]' '[a-z]')
 DISTPLUS="minos-float.fs theseus*.fs COPYING LGPLv3"
+DRAGONDIST = pattern/dragon{,-back,-head,-wing,-claw}.png \
+	pattern/{bark,normal-w1,back,focus}.png
+ICONDIST = icons/*.png
 
-tar zcf vfx-minos-$DATE.tar.gz *.m $(for i in *.m; do [ -f ${i%m}fs ] && echo ${i%m}fs; done) $(vfxlin include startx.fs save xvfxlin bye | grep Including | cut -f2 -d' ') $DISTPLUS icons/*.png
+tar zcf vfx-minos-$DATE.tar.gz *.m $(for i in *.m; do [ -f ${i%m}fs ] && echo ${i%m}fs; done) $(vfxlin include startx.fs save xvfxlin bye | grep Including | cut -f2 -d' ') $DISTPLUS $DRAGONDIST $ICONDIST
