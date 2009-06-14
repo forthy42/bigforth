a0f4e26cc40743a5b8f51e7eb0d3a78290c276ed
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2009-06-14 21:32:06 +0000

    Better distribution mechanism
    git-svn-id: https://forth-ev.de/repos/bigforth@1263 3b8d8251-53f3-0310-8f3b-fd1cb8370982

--- vfxdist.sh
+++ vfxdist.sh
@@ -2,8 +2,9 @@
 
 DATE=$(LC_ALL=C date '+%d%b%Y' | tr '[A-Z]' '[a-z]')
 DISTPLUS="minos-float.fs theseus*.fs COPYING LGPLv3"
-DRAGONDIST = pattern/dragon{,-back,-head,-wing,-claw}.png \
-	pattern/{bark,normal-w1,back,focus}.png
-ICONDIST = icons/*.png
+DRAGONDIST="$(echo pattern/dragon{,-back,-head,-wing,-claw}.png pattern/{bark,normal-w1,back,focus}.png)"
+ICONDIST=icons/*.png
+HELPERS="$(grep ^include *.m | cut -f2 -d' ' | sort -u)"
+MINOSCORE="$(vfxlin include startx.fs save xvfxlin bye | grep Including | cut -f2 -d' ')"
 
-tar zcf vfx-minos-$DATE.tar.gz *.m $(for i in *.m; do [ -f ${i%m}fs ] && echo ${i%m}fs; done) $(vfxlin include startx.fs save xvfxlin bye | grep Including | cut -f2 -d' ') $DISTPLUS $DRAGONDIST $ICONDIST
+tar zcf vfx-minos-$DATE.tar.gz *.m $MINOSCORE $DISTPLUS $DRAGONDIST $ICONDIST $HELPERS
