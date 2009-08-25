1c7baa8a7f9c753e37d7023820127e8f7e0fd235
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2009-08-25 20:55:47 +0000

    Use 0 arg for command name
    git-svn-id: https://forth-ev.de/repos/bigforth@1411 3b8d8251-53f3-0310-8f3b-fd1cb8370982

--- theseus.fs
+++ theseus.fs
@@ -1436,9 +1436,7 @@ Variable $acc
 
 : run-minos
     cur save-state @  IF  auto-save-minos  THEN
-    [defined] bigForth [IF] s" xbigforth '" [THEN]
-    [defined] VFXForth [IF] s" xvfxlin '" [THEN]
-    $acc $!
+    0 arg $acc $! s"  '" $acc $+!
     cur save-state @  cur file-name @ 0= or
     IF    auto-save-add
     ELSE  cur file-name $@ +$  THEN  s" '" +$
