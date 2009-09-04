569d983790f51fc365f2f305efa8bccba752ba4a
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2009-09-04 08:55:12 +0000

    make DOES> faster
    git-svn-id: https://forth-ev.de/repos/bigforth@1424 3b8d8251-53f3-0310-8f3b-fd1cb8370982

---
+++ does-comp.fs
@@ -0,0 +1,16 @@
+\ does compiler
+
+\ compiles DOES> in a Gforth-like way to avoid code/data problems
+
+: does? ( xt -- xt flag )
+    dup c@ $E8 = IF
+	dup cfa@ 2@ $8B50F48704C68306. d=
+    ELSE  false  THEN ;
+
+: does, ( xt -- xt / early exit )
+    does? IF
+	$68 c, here relon here 14 + ,
+	$68 c, here relon dup >body , cfa@ compile, here 5 - dup c@ 1+ swap c!
+	rdrop  EXIT  THEN ;
+
+' does, is 'cfa,
